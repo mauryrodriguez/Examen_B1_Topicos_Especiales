a48587c24659b9e1a905d549f6ac89808ca182ab
mauryrodriguez <maus_lm@hotmail.com>
2021-01-15 23:54:49 -0500

    login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,36 @@
 import {Component, OnInit} from '@angular/core';
+import { AuthService } from '../servicios/auth.service';
+import { ChatsService} from '../servicios/chats.service';
 
+interface chat {
+  description: string
+  name: string
+  id: string
+  img: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   public chatRooms: any = [];
 
-  constructor() {}
+  constructor(public authservice: AuthService, public chatservice: ChatsService) {}
+  Onlogout(){
+    this.authservice.logout();
+  }
+
+  ngOnInit(){
+    this.chatservice.getChatRooms().subscribe(chats => {
+      chats.map( chat => {
+        const  data: chat = chat.payload.doc.data() as chat;
+        data.id = chat.payload.doc.id;
+        console.log(data);
+      })
+    })
   }
 }
-
 
